79c3ed9f8e47c96cbbd7bd4d993b98c0df1067e7
matthew voorhees <voorhees.matthew@gmail.com>
2018-09-07 16:53:20 -0700

    Complete project upto Phase Id: #initialize

--- active_record_lite/part_1/p_1_skeleton/lib/01_sql_object.rb
+++ active_record_lite/part_1/p_1_skeleton/lib/01_sql_object.rb
@@ -1,22 +1,36 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+require 'byebug'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
   def self.columns
-    # ...
+    # debugger
+    return if @columns
+    @columns = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    @columns.first.map!(&:to_sym)
   end
 
   def self.finalize!
+    self.columns.each do |col|
+      define_method(col) {self.attributes[col]}
+      define_method("#{col}=") {|val| self.attributes[col] = val}
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    self.name.downcase + "s"
   end
 
   def self.all
@@ -32,11 +46,11 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    @params = params
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
